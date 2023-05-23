17d972f1a2a06d42ea8cb9d3a713c34604bec18d
Kaushal Kotkar <84152288+kaushal7208@users.noreply.github.com>
2023-05-23 14:48:12 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include ("partials/head.php");
+?>
+<body class="animsition">
+	<?php
+	include ("partials/header.php");
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Video Slider</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width , initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<br>
+	<br>
+	<br>
+	<section>
+		<video id="slider" autoplay loop controls>
+			<source src="video/video.mp4" type="video/mp4">
+		</video>
+		<ul class="navigation">
+			
+			<li onclick="videoselector('video/video.mp4')"><img src="images/4.png"  height="87px" width="140px" controls></li>
+			<li onclick="videoselector('video/video2.mp4')"><img src="images/5.png" height="87px" width="140px"controls></li>
+			<li onclick="videoselector('video/video3.mp4')"><img src="images/6.png" height="87px" width="140px"controls></li>
+			
+		</ul>
+	</section>
+	<script type="text/javascript">
+		function videoselector (videoLink) {
+			document.getElementById('slider').src=videoLink;
+		}
+	</script>
+</body>
+</html>
+	<!-- Title page
+	<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('images/about1.jpg');">
+		<h2 class="ltext-105 cl0 txt-center">
+			About
+		</h2>
+	</section>	 -->
+
+
+	<!-- Content page -->
+	<section class="bg0 p-t-75 p-b-120">
+		<div class="container">
+			<div class="row p-b-148">
+				<div class="col-md-7 col-lg-8">
+					<div class="p-t-7 p-r-85 p-r-15-lg p-r-0-md">
+						<h3 class="mtext-111 cl2 p-b-16">
+							Our Story
+						</h3>
+
+						<p class="stext-113 cl6 p-b-26">
+						In today's world we saw various sector are using Internet for growing their business this should be also be possible in case of agriculture. Farmers would be able to purchase the necessary equipment at cheaper prices. This website provides an easy access to every agriculture research and new techniques. There is no website available for farmers in India. This website will delivery right information and regular updates in agriculture related events. The website is feasible. Anyone can access it just by clicking on the website making it more reasonable and helpful. A well design website it helps user to get information with few clicks. This project will help the farmers/user to have a better communication. Farmers would be able to purchase the necessary equipment at cheaper prices.
+
+						</p>
+
+						<p class="stext-113 cl6 p-b-26">
+							Our customers are our top priority and we do not want to lose any of you. So please drive safe and never text while on the wheel. It is dangerous.
+						</p>
+
+						
+					</div>
+				</div>
+
+				<div class="col-11 col-md-5 col-lg-4 m-lr-auto">
+					<div class="how-bor1 ">
+						<div class="hov-img0">
+							<img src="images/About 3.png" alt="IMG">
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="order-md-2 col-md-7 col-lg-8 p-b-30">
+					<div class="p-t-7 p-l-85 p-l-15-lg p-l-0-md">
+						<h3 class="mtext-111 cl2 p-b-16">
+							Our Mission
+						</h3>
+
+						<p class="stext-113 cl6 p-b-26">
+							Our mission is to get the young people interested in agriculture, to encourage them to stay in the village, farmers must modernize agriculture. They must build up modern farm businesses that will ensure a better future for the young. This website will help farmers gain knowledge about farming methods. This website will provide a marketplace for sellers/public to trade/sell their old products at cheap rates. This website will also help reduce scams related to farming 
+
+						</p>
+
+						<div class="bor16 p-l-29 p-b-9 m-t-22">
+							<p class="stext-114 cl6 p-r-40 p-b-11">
+								<!-- After this, we will be able to control all the clothing industry like windows control the IT industry. -->
+							</p>
+
+							<span class="stext-111 cl8">
+								- Kaushal
+							</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="order-md-1 col-11 col-md-5 col-lg-4 m-lr-auto p-b-30">
+					<div class="how-bor2">
+						<div class="hov-img0">
+							<img src="images/About 2.png" alt="IMG">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br>
+		<br>
+		<br><br>
+	</section>	
+	
+
+<!-- <div class=""
+</div> -->
+
+
+	 <br><br><br>
+	<br>
+	<br>
+	<br><br><br>
+	<br>
+	<br>
+	<br><br><br>
+	<br>
+	<br>
+  <br><br><br>
+	
+  <center>
+    <style >
+
+.fcc-btn {
+  background-color: #717fe0;
+  font-family: Poppins-Medium;
+  font-size: 15px;
+  color: white;
+  padding: 15px 25px;
+  text-decoration: none;
+}
+    </style>
+   <a target="_blank" class="fcc-btn" href="https://chng.it/q5gBh9KmcC">Petition Form</a> 
+   <!-- <a class="btn btn-primary" href="https://www.freecodecamp.org/" target="_blank">Petition Form</a>   -->
+
+  </center>
+  <br>
+	<br>
+		
+
+	<!-- Footer -->
+	
+</body>
+</html>
+
+<?php
+	include('partials/footer.php');
+	?>
+	
---
+++ farming.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<body class="animsition">
+
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <!-- <meta name="description" content="" />
+        <meta name="author" content="" /> -->
+        <title>Info</title>
+        <link rel="icon" type="image/png" href="images/icons/favicon1.ico"/>
+        
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#page-top">Farmer 2.0</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#projects">Animal Husbandry</a></li>
+                       
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+                <div class="d-flex justify-content-center">
+                    <div class="text-center">
+                        <h1 class="mx-auto my-0 text-uppercase">Jai Jawaan Jai Kisaan</h1>
+                        <h2 class="text-white-50 mx-auto mt-2 mb-5">Help the farmers</h2>
+                        <a class="btn btn-primary" href="#about">Get Started</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- About-->
+        <section class="about-section text-center" id="about">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2 class="text-white mb-4">If agriculture goes wrong, nothing else will have a chance to go right</h2>
+                        <p class="text-white-50">
+                            Without agriculture, we would starve and the modern civilisation would come to a stop. Therefore sustainable agriculture, climate-smart agriculture, modern genetics and improved 
+                            <a href="https://www.simple.wikipedia.org/wiki/Farming">farming </a>
+                            methods, among others, are needed to ensure global food security.
+                        </p>
+                    </div>
+                </div>
+                <img class="img-fluid" src="assets/img/ipad.png" alt="..." />
+            </div>
+        </section>
+        <!-- Projects-->
+        <section class="projects-section bg-light" id="projects">
+            <div class="container px-4 px-lg-5">
+                <!-- Featured Project Row-->
+                <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="assets/img/bg-masthead.jpg" alt="..." /></div>
+                    <div class="col-xl-4 col-lg-5">
+                        <div class="featured-text text-center text-lg-left">
+                            <h4>Animal Husbandry</h4>
+                            <p class="text-black-50 mb-0">Animal husbandry is the branch of agriculture concerned with animals that are raised for meat, fibre, milk, eggs, or other products. It includes day-to-day care, selective breeding and the raising of livestock. Husbandry has a long history, starting with the Neolithic revolution when animals were first domesticated, from around 13,000 BC onwards, antedating farming of the first crops. By the time of early civilisations such as ancient Egypt, cattle, sheep, goats and pigs were being raised on farms.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Project One Row-->
+                <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-01.jpg" alt="..." /></div>
+                    <div class="col-lg-6">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-left">
+                                    <h4 class="text-white">Dairy Farming</h4>
+                                    <p class="mb-0 text-white-50">Dairy farming is the agricultural technique concerned with the long term production of milk, which is then processed to obtain dairy products such as curd, cheese, yoghurt, butter, cream, etc. It involves the management of dairy animals such as cows, buffaloes, sheep, goat, etc.
+                                        A healthy animal is physically, mentally and socially sound.
+                                        These animals are milked by hand or by machines. The milk is preserved and converted into dairy products industrially, which are then used for commercial purposes.</p>
+                                    <hr class="d-none d-lg-block mb-0 ms-0" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <!-- Project Two Row-->
+                <div class="row gx-0 justify-content-center">
+                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-02.jpg" alt="..." /></div>
+                    <div class="col-lg-6 order-lg-first">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-right">
+                                    <h4 class="text-white">Poultry Farming</h4>
+                                    <p class="mb-0 text-white-50">Poultry farming is concerned with raising and breeding of birds for commercial purposes. Birds like ducks, chickens, geese, pigeons, turkeys, etc. are domesticated for eggs and meat.
+                                        It is very important to take care of the animals and maintain them in a disease-free environment to obtain healthy food from them. The eggs and meat are a rich source of protein.
+                                        The faeces of birds are used as manure to improve soil fertility. Poultry farming provides employment to a large number of people and helps in improving the economy of the farmers.
+                                        </p>
+                                    <hr class="d-none d-lg-block mb-0 me-0" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Project Three Row -->
+                <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-03.jpg" alt="..." /></div>
+                    <div class="col-lg-6">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-left">
+                                    <h4 class="text-white">Fish Farming</h4>
+                                    <p class="mb-0 text-white-50">Fish farming is the process of raising fish in closed tanks or ponds for commercial purposes. There is an increasing demand for fish and fish protein. Fish species such as salmon, catfish, cod, and tilapia are raised in fish farms.
+                                        Fish farming or pisciculture is of two types:
+                                        Extensive aquaculture, based on the local photosynthetic production
+                                        Intensive aquaculture, based on the external food supply given to fishes.</p>
+                                    <hr class="d-none d-lg-block mb-0 ms-0" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Project Four Row-->
+                <div class="row gx-0 justify-content-center">
+                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-04.jpg" alt="..." /></div>
+                    <div class="col-lg-6 order-lg-first">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-right">
+                                    <h4 class="text-white">Bee Farming</h4>
+                                    <p class="mb-0 text-white-50">Bee farming or apiculture is the practice of maintaining bee colonies by humans in man-made hives. Honey bees are reared on a large scale. The bees are domesticated for honey, wax, and to pollinate flowers. They are also used by other beekeepers for the same purposes. The place where bees are kept is known as an apiary or a bee yard.</p>
+                                    <hr class="d-none d-lg-block mb-0 me-0" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
+        </section>
+        
+        <!-- Signup-->
+        
+        <!-- Footer-->
+        <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; Pranav 2021</div></footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+   
+    </body>
+</html>
